ea01b7d65303b7ee28eab513a7c6ce6e264fbcc8
Nadja-BeCode <nadja.agostinho@gmail.com>
2021-03-14 16:42:40 +0100

    update didn't finish

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,2 @@
 
-let name = prompt("Would you like to play the Hangman game?");
-//console.log(prompt);
 
-let words = ["javascript","hangman"];
-let word = words[Math.floor(Math.random()*words.length)];
-
-let answerWord = [];
-for(let i = 0; i = word.length; i++){
-    answerWord[i] = "_";
-}
-console.log(answerWord[i]);
-
-let guess = promt("Guess a letter");
